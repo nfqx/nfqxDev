0e08fc680a0f12116825336d3f9051632dc65a8d
Stanley Yin <info@cloudfactory.mt>
2026-02-19 22:55:20 +0100

    NFQXSO-30 HTML, JS, Apex Stubs; Labels

--- force-app/main/default/lwc/projectPlanningCalendar/projectPlanningCalendar.js
+++ force-app/main/default/lwc/projectPlanningCalendar/projectPlanningCalendar.js
@@ -1,3 +1,45 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class ProjectPlanningCalendar extends LightningElement {}
+import projectPlanning from '@salesforce/label/c.GeneralLabelProjectPlanning';
+import changeTimeFrame from '@salesforce/label/c.GeneralLabelChangeTimeFrame';
+import employeeMode from '@salesforce/label/c.GeneralLabelEmployeeMode';
+import saveLabel from '@salesforce/label/c.Save';
+import employee from '@salesforce/label/c.GeneralLabelEmployee';
+import project from '@salesforce/label/c.GeneralLabelProject';
+import calendarWeekAbbreviation from '@salesforce/label/c.GeneralLabelCalendarWeekAbbreviation';
+
+import retrieveCalendarData from "@salesforce/apex/ProjectPlanningController.retrieveCalendarData";
+import retrieveCurrentWeeks from "@salesforce/apex/ProjectPlanningController.retrieveCurrentWeeks";
+
+export default class ProjectPlanningCalendar extends LightningElement {
+    label = {
+        projectPlanning,
+        changeTimeFrame,
+        employeeMode,
+        saveLabel,
+        employee,
+        project,
+        calendarWeekAbbreviation
+    };
+
+    @track dataLoaded = false;
+    @track hasData = false;
+    @track selectedWeeks = [];
+    @track projectEmployeeData = [];
+
+    @track showModal = false;
+    @track selectedPlanningForModal = null;
+
+    connectedCallback(){
+        retrieveCurrentWeeks()
+        .then(result => {
+            this.selectedWeeks = JSON.parse(JSON.stringify(result));
+            retrieveCalendarData({weeks: this.selectedWeeks})
+            .then(innerResult => {
+                this.projectEmployeeData = JSON.parse(JSON.stringify(innerResult));
+                this.dataLoaded = true;
+                this.hasData = this.projectEmployeeData.length > 0;
+            })
+        });
+    }
+}
--- force-app/main/default/lwc/projectPlanningEmployeeOverview/projectPlanningEmployeeOverview.js
+++ force-app/main/default/lwc/projectPlanningEmployeeOverview/projectPlanningEmployeeOverview.js
@@ -1,3 +1,40 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class ProjectPlanningEmployeeOverview extends LightningElement {}
+import employeesOverview from '@salesforce/label/c.GeneralLabelEmployeesOverview';
+import changeTimeFrame from '@salesforce/label/c.GeneralLabelChangeTimeFrame';
+import employee from '@salesforce/label/c.GeneralLabelEmployee';
+import project from '@salesforce/label/c.GeneralLabelProject';
+import projectMode from '@salesforce/label/c.GeneralLabelProjectMode';
+import calendarWeekAbbreviation from '@salesforce/label/c.GeneralLabelCalendarWeekAbbreviation';
+
+import retrieveEmployeeData from "@salesforce/apex/ProjectPlanningController.retrieveEmployeeData";
+import retrieveCurrentWeeks from "@salesforce/apex/ProjectPlanningController.retrieveCurrentWeeks";
+
+export default class ProjectPlanningEmployeeOverview extends LightningElement {
+    label = {
+        employeesOverview,
+        changeTimeFrame,
+        employee,
+        project,
+        projectMode,
+        calendarWeekAbbreviation
+    };
+
+    @track dataLoaded = false;
+    @track hasData = false;
+    @track selectedWeeks = [];
+    @track employeeProjectData = [];
+
+    connectedCallback(){
+        retrieveCurrentWeeks()
+        .then(result => {
+            this.selectedWeeks = JSON.parse(JSON.stringify(result));
+            retrieveEmployeeData({weeks: this.selectedWeeks})
+            .then(innerResult => {
+                this.employeeProjectData = JSON.parse(JSON.stringify(innerResult));
+                this.dataLoaded = true;
+                this.hasData = this.employeeProjectData.length > 0;
+            })
+        });
+    }
+}
